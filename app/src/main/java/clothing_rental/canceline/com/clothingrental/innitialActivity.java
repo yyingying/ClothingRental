dea2c42cd7411786b4a6d95c33711c6e3568f28d
zhangyu <975135274@qq.com>
2018-02-04 13:52:31 +0800

    config:RxJava2 & kotlin

--- app/src/main/java/clothing_rental/canceline/com/clothingrental/innitialActivity.java
+++ app/src/main/java/clothing_rental/canceline/com/clothingrental/innitialActivity.java
@@ -1,7 +1,6 @@
 package clothing_rental.canceline.com.clothingrental;
 
 
-import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
