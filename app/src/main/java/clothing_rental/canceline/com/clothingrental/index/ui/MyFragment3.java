edf27ab85e05e8c3efaa0334e53495ab0ad535aa
yyingying <zxc583911632@qq.com>
2018-02-07 17:54:51 +0800

    keyi

---
+++ app/src/main/java/clothing_rental/canceline/com/clothingrental/index/ui/MyFragment3.java
@@ -0,0 +1,30 @@
+package clothing_rental.canceline.com.clothingrental.index.ui;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import clothing_rental.canceline.com.clothingrental.R;
+
+/**
+ * Created by kingShin on 2018/2/7.
+ */
+
+public class MyFragment3 extends Fragment {
+    public MyFragment3() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fg_content, container, false);
+        TextView txt_content = (TextView) view.findViewById(R.id.txt_content);
+        txt_content.setText("第一个Fragment");
+        Log.e("HEHE", "1日狗");
+        return view;
+    }
+}
